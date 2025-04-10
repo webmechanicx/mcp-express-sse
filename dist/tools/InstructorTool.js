b4cb6be8629ee729c5ee749ab716defb056a1afc
Farhadur Rahim <webmechanicx@gmail.com>
2025-04-10 22:51:38 +0600

    add mcp-framework

---
+++ dist/tools/InstructorTool.js
@@ -0,0 +1,37 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mcp_framework_1 = require("mcp-framework");
+require("dotenv/config");
+class InstructorTool extends mcp_framework_1.MCPTool {
+    constructor() {
+        super(...arguments);
+        this.name = "instructor";
+        this.description = "Get the list of all Instructors tool description.";
+        this.schema = {};
+    }
+    getAuthHeader() {
+        return `Bearer ${process.env.API_TOKEN}`;
+    }
+    execute() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const response = yield this.fetch(`${process.env.API_HOST}/instructor`, {
+                headers: {
+                    //Authorization: this.getAuthHeader(),
+                    "Content-Type": "application/json",
+                },
+            });
+            return response;
+        });
+    }
+}
+exports.default = InstructorTool;
+//# sourceMappingURL=InstructorTool.js.map
